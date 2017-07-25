0546762bccd28e6173a20a29a71ac26bbcbd5a8f
asarnaout <asarnaout@hotmail.com>
2017-07-25 04:57:46 +0200

    Referencing the correct directory for the broker client after moving it to the services directory

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const config = require('./config.js');
 const app = require('express')();
 const handleRoutes = require('./routes.js');
 const amqp = require('amqplib/callback_api');
-const brokerClient = new (require('./broker-client.js'))(amqp, config);
+const brokerClient = new (require('./services/broker-client.js'))(amqp, config);
 const webSocketServer = require('./websocket-server.js');
 
 setupServer = async () => {
