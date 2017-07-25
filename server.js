dcd38f55fb32e8dfc3c9d7cca3dd7786f80f93af
asarnaout <asarnaout@hotmail.com>
2017-07-25 04:52:53 +0200

    Adding a new websocket server to handle communication with clients

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const app = require('express')();
 const handleRoutes = require('./routes.js');
 const amqp = require('amqplib/callback_api');
 const brokerClient = new (require('./broker-client.js'))(amqp, config);
-
+const webSocketServer = require('./websocket-server.js');
 
 setupServer = async () => {
 	app.use(bodyParser.json());
 	await brokerClient.setupBrokerConnection();
+	webSocketServer.setupWebSocketServer();
 	app.listen(config.port);
 	console.log('elite-chat: listening to connections on port: ' + config.port);
 	handleRoutes(app, brokerClient);
 }
 
 setupServer();
-
 
---
+++ websocket-server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const config = require('./config.js');
+var connectedClients = [];
+
+var webSocketServer = {
+	setupWebSocketServer: () => {
+		var server = http.createServer(function(request, response) {});
+
+		server.listen(config.webSocketServerPort, function() {
+			console.log('web socket server is now running on port: ' + config.webSocketServerPort);
+		});
+
+		wsServer = new (require('websocket').server)({
+			httpServer: server
+		});
+
+		wsServer.on('request', function(r){
+			var connection = r.accept('echo-protocol', r.origin);
+			connectedClients.push(connection);
+		});
+	},
+	broadcast: (message) => {
+		for(var i in clients){
+			clients[i].sendUTF(message);
+		}
+	}
+};
+
+module.exports = webSocketServer;
