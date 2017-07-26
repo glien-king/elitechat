a709f78c664be11e6a2ea145483db08c7c48466b
asarnaout <asarnaout@hotmail.com>
2017-07-26 07:47:23 +0200

    Serving a favicon

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const handleRoutes = require('./routes.js');
 const amqp = require('amqplib/callback_api');
 const webSocketServer = require('./websocket-server.js');
+const favicon = require('serve-favicon');
 const brokerClient = new (require('./services/broker-client.js'))(amqp, config, webSocketServer);
 const path = require('path')
 
@@ -14,6 +15,7 @@ setupServer = async () => {
 	app.use(bodyParser.json());
 	app.use("/styles", express.static(__dirname + '/views/css'));
 	app.use("/scripts", express.static(__dirname + '/views/js'));
+	app.use(favicon(path.join(__dirname, '/', 'favicon.ico')));
 	await brokerClient.setupBrokerConnection();
 	await mongoClient.initializeDbConnection(config);	
 	webSocketServer.setupWebSocketServer();
