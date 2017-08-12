8b6ee84df3c4306c96bec6d0727189b28926625a
asarnaout <asarnaout@hotmail.com>
2017-08-12 12:42:10 +0200

    Added separate consumers to perform expensive operations separately

---
+++ consumers/account_consumer.js
@@ -0,0 +1,19 @@
+const amqp = require('amqplib/callback_api');
+const config = require('../config.js');
+const mongoClient = require('../data/mongo-client.js');
+
+setupBrokerConnection = () => {
+	amqp.connect(config.rabbitMqEndpoint, function(err, conn) {			
+		conn.createChannel(function(err, ch) {
+			ch.assertQueue(config.accountQueueName, {durable: false});
+			ch.consume(config.accountQueueName, consumeMessage, {noAck: true});
+		});
+	});	
+};
+
+
+consumeMessage = (message) => {
+	var content = message.content.toString();
+}
+
+setupBrokerConnection();
---
+++ consumers/message_consumer.js
@@ -0,0 +1,19 @@
+const amqp = require('amqplib/callback_api');
+const config = require('../config.js');
+const mongoClient = require('../data/mongo-client.js');
+
+setupBrokerConnection = () => {
+	amqp.connect(config.rabbitMqEndpoint, function(err, conn) {			
+		conn.createChannel(function(err, ch) {
+			ch.assertQueue(config.messagingQueueName, {durable: false});
+			ch.consume(config.messagingQueueName, consumeMessage, {noAck: true});
+		});
+	});	
+};
+
+
+consumeMessage = (message) => {
+	var content = message.content.toString();
+}
+
+setupBrokerConnection();
